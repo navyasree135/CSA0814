7b21588d9b30bbfe1fde77c908de7ae9237ef61c
navyasree135 <160712866+navyasree135@users.noreply.github.com>
2024-08-08 09:47:24 +0530

    hallow pyramid.py

---
+++ Day 5/hallow pyramid.py
@@ -0,0 +1,11 @@
+n=10
+for i in range(n):
+  for j in range(n-i):
+    print(' ', end='')
+
+  for j in range(2*i+1):
+    if j==0 or j==2*i or i==n-1:
+      print('*', end='')
+    else:
+      print(' ', end='')
+  print()
