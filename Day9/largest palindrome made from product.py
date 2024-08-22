5c45c82afff4a2103926727d2f80bd2cce64deb8
navyasree135 <160712866+navyasree135@users.noreply.github.com>
2024-08-22 09:05:54 +0530

    Update largest palindrome made from product.py

--- Day9/largest palindrome made from product.py
+++ Day9/largest palindrome made from product.py
@@ -1,3 +1,3 @@
 def largest_palindrome():
-  return max(p for i in range(1000, 10000) for i in range(i, 10000) if str(p:=i*j)== str(p)[::-1])
-  print(largest_palindrome())
+    return max(i * j for i in range(1000, 10000) for j in range(i, 10000) if str(i * j) == str(i * j)[::-1])
+print(largest_palindrome())
