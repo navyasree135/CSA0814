c410db7f32fcb06b94a4d91482c8a4292ffd4c0a
navyasree135 <160712866+navyasree135@users.noreply.github.com>
2024-08-26 10:42:16 +0530

    minimum number of jumps.py

--- Day11/minimum number of jumps.py
+++ Day11/minimum number of jumps.py
@@ -1,15 +1,14 @@
-def min_jumps(arr):
-    n = len(arr)
-    if n <= 1:
-        return 0    
-    jumps, farthest, current_end = 0, 0, 0    
+arr = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]
+n = len(arr)
+if n <= 1:
+    jumps = 0
+else:
+    jumps, farthest, current_end = 0, 0, 0
     for i in range(n - 1):
         farthest = max(farthest, i + arr[i])
         if i == current_end:
             jumps += 1
             current_end = farthest
             if current_end >= n - 1:
-                break   
-    return jumps
-arr = [1,3,5,8,9,2,6,7,6,8,9]
-print("Minimum number of jumps:", min_jumps(arr))
+                break
+print("Minimum number of jumps:", jumps)
