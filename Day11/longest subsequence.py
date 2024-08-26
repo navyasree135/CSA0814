0c10819ec4e38a06ddee2cbc4853108c83c6901f
navyasree135 <160712866+navyasree135@users.noreply.github.com>
2024-08-26 10:35:20 +0530

    longest subsequence.py

--- Day11/longest subsequence.py
+++ Day11/longest subsequence.py
@@ -1,23 +1,16 @@
-def longest_increasing_subsequence(arr):
-    if not arr:
-        return 0, []
-    longest_len = 0
-    longest_seq = []
-    current_seq = []
-    for i in arr:
-        if not current_seq or i > current_seq[-1]:
-            current_seq.append(i)
-        else:
-            if len(current_seq) > longest_len:
-                longest_len = len(current_seq)
-                longest_seq = current_seq
-            current_seq = [i]    
-    if len(current_seq) > longest_len:
-        longest_len = len(current_seq)
-        longest_seq = current_seq
-
-    return longest_len, longest_seq
 arr = [10, 22, 9, 33, 41, 50, 41, 60]
-length, sequence = longest_increasing_subsequence(arr)
-print("Length:", length)
-print("Sequence:", sequence)
+longest_len, longest_seq = 0, []
+current_seq = []
+for i in arr:
+    if not current_seq or i > current_seq[-1]:
+        current_seq.append(i)
+    else:
+        if len(current_seq) > longest_len:
+            longest_len = len(current_seq)
+            longest_seq = current_seq
+        current_seq = [i]
+if len(current_seq) > longest_len:
+    longest_len = len(current_seq)
+    longest_seq = current_seq
+print("Length:", longest_len)
+print("Sequence:", longest_seq)
